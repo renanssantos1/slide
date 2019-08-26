595a2e30bf9748a2e1adfb57b253065e8fdebcb1
Renan da Silva <renan.serverti@gmail.com>
2019-08-26 18:32:43 -0300

    ADD to next prev methods

--- js/slide.js
+++ js/slide.js
@@ -5,6 +5,10 @@ export default class Slide {
     this.dist = { finalPosition: 0, startX: 0, movement: 0 }
   }
 
+  transition(active) {
+    this.slide.style.transition = active ? 'transform .3s' : '';
+  }
+
   moveSlide(distX) {
     this.dist.movePosition = distX;
     this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
@@ -26,11 +30,11 @@ export default class Slide {
       movetype = 'touchmove';
     }
     this.wrapper.addEventListener(movetype, this.onMove);
+    this.transition(false);
   }
 
   onMove(event) {
-    const pointerPosition = (event.type === 'mousemove') 
-      ? event.clientX : event.changedTouches[0].clientX;
+    const pointerPosition = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0].clientX;
     const finalPosition = this.updatePosition(pointerPosition);
     this.moveSlide(finalPosition);
   }
@@ -39,6 +43,18 @@ export default class Slide {
     const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
     this.wrapper.removeEventListener(movetype, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
+    this.transition(true);
+    this.changeSlideOnEnd();
+  }
+
+  changeSlideOnEnd() {
+    if (this.dist.movement > 120 && this.index.next !== undefined) {
+      this.activeNextSlide();
+    } else if (this.dist.movement < -120 && this.index.prev !== undefined) {
+      this.activePrevSlide();
+    } else {
+      this.changeSlide(this.index.active);
+    }
   }
 
   addSlideEvents() {
@@ -54,52 +70,49 @@ export default class Slide {
     this.onEnd = this.onEnd.bind(this);
   }
 
+  // Slides config
 
-  // metodo que calcula
-  // a posição do item
-  // e o coloca no centro do display
-  slidePosition(slide){
-    // total da tela menos o total do item / 2
+  slidePosition(slide) {
     const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
     return -(slide.offsetLeft - margin);
   }
 
-  // Slides config
-  // pega a posição do slide
-  // salva a posição da imagem
-  slideConfig(){
+  slidesConfig() {
     this.slideArray = [...this.slide.children].map((element) => {
-      const position = this.slidePosition(element)
-      return {
-        position,
-        element,
-      }
+      const position = this.slidePosition(element);
+      return { position, element };
     });
   }
 
-  // saber quando chegou no fim
-  // ou no inicio do slide.
-  slideIndexNav(index){
+  slidesIndexNav(index) {
     const last = this.slideArray.length - 1;
     this.index = {
-      prev: index ? -1 : undefined,
+      prev: index ? index - 1 : undefined,
       active: index,
-      next : index === last ? undefined : index + 1,
-    };
+      next: index === last ? undefined : index + 1,
+    }
   }
 
-  // metodo que muda o slide, de acordo
-  // com o index que passarmos para ele
-  changeSlide(index){
-    const activeSlide = this.slideArray[index].position;
-    this.moveSlide(activeSlide);
+  changeSlide(index) {
+    const activeSlide = this.slideArray[index];
+    this.moveSlide(activeSlide.position);
+    this.slidesIndexNav(index);
     this.dist.finalPosition = activeSlide.position;
+  }
+
+  activePrevSlide() {
+    if (this.index.prev !== undefined) this.changeSlide(this.index.prev);
+  }
+
+  activeNextSlide() {
+    if (this.index.next !== undefined) this.changeSlide(this.index.next);
   }
 
   init() {
     this.bindEvents();
+    this.transition(true);
     this.addSlideEvents();
-    this.slideConfig();
+    this.slidesConfig();
     return this;
   }
 }
